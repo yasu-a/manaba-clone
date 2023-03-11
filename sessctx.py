f341de37218621eac9e86b0c9c220b3e32857504
yasu-a <yasuhiro.gtgt@gmail.com>
2023-03-12 02:02:44 +0900

    add do_commit parameter

--- sessctx.py
+++ sessctx.py
@@ -15,11 +15,20 @@ import app_logging
 class SessionContext:
     logger = app_logging.create_logger()
 
-    def __init__(self, session_class):
+    def __init__(self, session_class, do_commit=None):
         self.__session_class = session_class
+        self.__do_commit = do_commit
+
+    @staticmethod
+    def __eval_prioritized_values(values, *, default):
+        for value in values:
+            if value is None:
+                continue
+            return value
+        return default
 
     @contextlib.contextmanager
-    def __call__(self) -> Iterable[Session]:
+    def __call__(self, *, do_commit=None) -> Iterable[Session]:
         session: Session = self.__session_class()
         session_index = hashlib.sha3_256(str(session).encode('utf-8')).hexdigest()[-8:]
         session_index = f'0x{session_index.upper()}'
@@ -31,8 +40,13 @@ class SessionContext:
             self.logger.debug(f'session {session_index} ROLLED BACK due to {e}')
             raise
         else:
-            session.commit()
-            self.logger.debug(f'session {session_index} COMMITTED')
+            do_commit_final = self.__eval_prioritized_values(
+                [do_commit, self.__do_commit],
+                default=True
+            )
+            if do_commit_final:
+                session.commit()
+                self.logger.debug(f'session {session_index} COMMITTED')
         finally:
             session.close()
             self.logger.debug(f'session {session_index} CLOSED')
@@ -45,7 +59,7 @@ class SessionContext:
         return SessionClass
 
     @classmethod
-    def create_instance(cls, db_path: str, base):
+    def create_instance(cls, db_path: str, base, **kwargs):
         SessionClass = cls.create_session_class(db_path, base)
-        cls.logger.info(f'session context create: {db_path=}, {base=}')
-        return cls(SessionClass)
+        cls.logger.info(f'session context created: {db_path=}, {base=}')
+        return cls(SessionClass, **kwargs)
