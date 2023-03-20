ee1371eecd32a8c9ed100b7b54c5ef92fe0c6041
yasu-a <yasuhiro.gtgt@gmail.com>
2023-03-21 02:13:01 +0900

    fixup! New sessctx

--- model/__init__.py
+++ model/__init__.py
@@ -1,11 +1,2 @@
 from .common import SQLDataModelMixin, SQLDataModelBase, create_timestamp
-
-_DATABASE_PATH = 'db/database.db'
-
-
-def create_session_context(custom_db_path=None):
-    from sessctx import SessionContext
-    return SessionContext.create_instance(
-        custom_db_path or _DATABASE_PATH,
-        SQLDataModelBase
-    )
+from .creator import create_session_context
